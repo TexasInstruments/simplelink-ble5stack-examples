afff1558cc4d93df1e799174526cc5b8fc0c4797
TI Wireless connectivity <e2e.ti.com>
2023-05-06 00:37:06 +0000

    simplelink_lowpower_f3_sdk 7_10_00_35 ble5stack examples 0506003706

---
+++ examples/rtos/LP_EM_CC2340R5/ble5stack/host_test/Application/host_test_app.h
@@ -0,0 +1,55 @@
+/******************************************************************************
+
+ @file  host_test_app.h
+
+ @brief This file contains the HostTestApp sample application definitions and
+        prototypes.
+
+ Group: WCS, BTS
+ $Target Device: DEVICES $
+
+ ******************************************************************************
+ $License: BSD3 2013 $
+ ******************************************************************************
+ $Release Name: PACKAGE NAME $
+ $Release Date: PACKAGE RELEASE DATE $
+ *****************************************************************************/
+
+#ifndef HOSTESTAPP_H
+#define HOSTESTAPP_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*********************************************************************
+ * INCLUDES
+ */
+
+/*********************************************************************
+ * CONSTANTS
+ */
+
+/*********************************************************************
+ * MACROS
+ */
+
+/*********************************************************************
+ * FUNCTIONS
+ */
+
+/*
+ * Task creation function for the Host Test App.
+ */
+extern void HostTestApp_createTask(void);
+
+
+/*********************************************************************
+*********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HOSTESTAPP_H */
