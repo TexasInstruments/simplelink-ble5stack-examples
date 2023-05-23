4231e1f8d2ed1023ac574bf4075dbe65b04f8e4f
TI Wireless connectivity <e2e.ti.com>
2023-05-23 18:23:27 +0000

    SimpleLink lpf3-7.10.00.35 ble5stack examples update (Docker commit)
    This commit updates ble5stack examples to SimpleLink F3 7.10.00.35 SDK release.

---
+++ examples/rtos/LP_EM_CC2340R5/ble5stack/basic_ble_ptm/app/app_main.c
@@ -0,0 +1,102 @@
+/******************************************************************************
+
+@file  app_main.c
+
+@brief This file contains the application main functionality
+
+Group: WCS, BTS
+$Target Device: DEVICES $
+
+******************************************************************************
+$License: BSD3 2022 $
+******************************************************************************
+$Release Name: PACKAGE NAME $
+$Release Date: PACKAGE RELEASE DATE $
+*****************************************************************************/
+
+//*****************************************************************************
+//! Includes
+//*****************************************************************************
+#include "ti_ble_config.h"
+#include <ti/bleapp/ble_app_util/inc/bleapputil_api.h>
+
+//*****************************************************************************
+//! Defines
+//*****************************************************************************
+
+//*****************************************************************************
+//! Globals
+//*****************************************************************************
+
+// Parameters that should be given as input to the BLEAppUtil_init function
+BLEAppUtil_GeneralParams_t appMainParams =
+{
+    .taskPriority = 1,
+    .taskStackSize = 1024,
+    .profileRole = (BLEAppUtil_Profile_Roles_e)HOST_CONFIG,
+    .addressMode = DEFAULT_ADDRESS_MODE,
+    .deviceNameAtt = attDeviceName,
+};
+
+BLEAppUtil_PeriCentParams_t appMainPeriCentParams =
+{
+#if defined( HOST_CONFIG ) && ( HOST_CONFIG & ( PERIPHERAL_CFG ) )
+ .connParamUpdateDecision = DEFAULT_PARAM_UPDATE_REQ_DECISION,
+#endif
+
+#if defined( HOST_CONFIG ) && ( HOST_CONFIG & ( PERIPHERAL_CFG | CENTRAL_CFG ) )
+ .gapBondParams = &gapBondParams
+#endif
+};
+
+//*****************************************************************************
+//! Functions
+//*****************************************************************************
+extern bStatus_t PTM_start(void);
+
+/*********************************************************************
+ * @fn      criticalErrorHandler
+ *
+ * @brief   Application task entry point
+ *
+ * @return  none
+ */
+void criticalErrorHandler(int32 errorCode , void* pInfo)
+{
+
+}
+
+/*********************************************************************
+ * @fn      App_StackInitDone
+ *
+ * @brief   This function will be called when the BLE stack init is
+ *          done.
+ *          It should call the applications modules start functions.
+ *
+ * @return  none
+ */
+void App_StackInitDoneHandler(gapDeviceInitDoneEvent_t *deviceInitDoneData)
+{
+    bStatus_t status = SUCCESS;
+
+    status = PTM_start();
+    if ( status != SUCCESS )
+    {
+    // TODO: Call Error Handler
+    }
+
+}
+
+/*********************************************************************
+ * @fn      appMain
+ *
+ * @brief   Application main function
+ *
+ * @return  none
+ */
+void appMain(void)
+{
+    // Call the BLEAppUtil module init function
+    BLEAppUtil_init(&criticalErrorHandler, &App_StackInitDoneHandler,
+                    &appMainParams, &appMainPeriCentParams);
+}
---
+++ examples/rtos/LP_EM_CC2340R5/ble5stack/basic_persistent/app/app_main.c
@@ -0,0 +1,111 @@
+/******************************************************************************
+
+@file  app_main.c
+
+@brief This file contains the application main functionality
+
+Group: WCS, BTS
+$Target Device: DEVICES $
+
+******************************************************************************
+$License: BSD3 2022 $
+******************************************************************************
+$Release Name: PACKAGE NAME $
+$Release Date: PACKAGE RELEASE DATE $
+*****************************************************************************/
+
+//*****************************************************************************
+//! Includes
+//*****************************************************************************
+#include "ti_ble_config.h"
+#include <ti/bleapp/ble_app_util/inc/bleapputil_api.h>
+#include <ti/bleapp/profiles/oad/oad_profile.h>
+#include <app_main.h>
+
+//*****************************************************************************
+//! Defines
+//*****************************************************************************
+
+//*****************************************************************************
+//! Globals
+//*****************************************************************************
+
+// Parameters that should be given as input to the BLEAppUtil_init function
+BLEAppUtil_GeneralParams_t appMainParams =
+{
+    .taskPriority = 1,
+    .taskStackSize = 1024,
+    .profileRole = (BLEAppUtil_Profile_Roles_e)HOST_CONFIG,
+    .addressMode = DEFAULT_ADDRESS_MODE,
+    .deviceNameAtt = attDeviceName,
+};
+
+BLEAppUtil_PeriCentParams_t appMainPeriCentParams =
+{
+ .connParamUpdateDecision = DEFAULT_PARAM_UPDATE_REQ_DECISION,
+ .gapBondParams = &gapBondParams
+};
+
+//*****************************************************************************
+//! Functions
+//*****************************************************************************
+
+/*********************************************************************
+ * @fn      criticalErrorHandler
+ *
+ * @brief   Application task entry point
+ *
+ * @return  none
+ */
+void criticalErrorHandler(int32 errorCode , void* pInfo)
+{
+//    trace();
+//
+//#ifdef DEBUG_ERR_HANDLE
+//
+//    while (1);
+//#else
+//    SystemReset();
+//#endif
+
+}
+
+/*********************************************************************
+ * @fn      App_StackInitDone
+ *
+ * @brief   This function will be called when the BLE stack init is
+ *          done.
+ *          It should call the applications modules start functions.
+ *
+ * @return  none
+ */
+void App_StackInitDoneHandler(gapDeviceInitDoneEvent_t *deviceInitDoneData)
+{
+    bStatus_t status = SUCCESS;
+
+    status = Connection_start();
+    status = Peripheral_start();
+
+    status = Pairing_start();
+
+    status = OADProfile_start(NULL);
+
+    if(status != SUCCESS)
+    {
+        // TODO: Call Error Handler for each call
+    }
+}
+
+/*********************************************************************
+ * @fn      appMain
+ *
+ * @brief   Application main function
+ *
+ * @return  none
+ */
+void appMain(void)
+{
+    // Call the BLEAppUtil module init function
+    BLEAppUtil_init(&criticalErrorHandler, &App_StackInitDoneHandler,
+                    &appMainParams, &appMainPeriCentParams);
+}
